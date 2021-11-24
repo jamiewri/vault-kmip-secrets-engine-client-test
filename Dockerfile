3f01c5ca9de407728659800fc23afbfe27d79ce7
jamiewri <jamiewri@me.com>
2021-11-25 08:05:09 +1100

    removing unused arg parsers

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.7.9-slim
 WORKDIR /usr/src/app
 COPY src/ .
 RUN pip install  --no-cache-dir -r requirements.txt
-CMD ["python", "./vault-kmip-test.py"]
+CMD ["python", "vault-kmip-test.py"]
--- src/vault-kmip-test.py
+++ src/vault-kmip-test.py
@@ -11,12 +11,6 @@ from kmip.pie import client
 if __name__ == '__main__':
     logger = utils.build_console_logger(logging.INFO)
 
-    # Build and parse arguments
-    parser = utils.build_cli_parser(enums.Operation.CREATE)
-    opts, args = parser.parse_args(sys.argv[1:])
-
-    config = opts.config
-
     algorithm = 'AES'
     length = 128
 
@@ -37,7 +31,6 @@ if __name__ == '__main__':
             key=key,
             ca=ca,
             config='client',
-            config_file=opts.config_file
     ) as client:
 
         # operation_create
@@ -45,7 +38,6 @@ if __name__ == '__main__':
             uid = client.create(
                 algorithm,
                 length,
-                operation_policy_name=opts.operation_policy_name
             )
             logger.info("Successfully created symmetric key with ID: "
                         "{0}".format(uid))
@@ -57,18 +49,44 @@ if __name__ == '__main__':
             logger.error(e)
             testResults["results"].append({"operationName": "operation_create",
                                            "successful": False})
-        # operation_activate
-        try:
-            result = client.activate(uid)
-            logger.info("Successfully activated: {0}".format(uid))
 
-            testResults["results"].append({"operationName": "operation_activate",
-                                           "successful": True})
-        
-        except Exception as e:
-            logger.error(e)
-            testResults["results"].append({"operationName": "operation_activate",
-                                           "successful": False})
+        # Commenting out `operation_activate` and `operation_revoke` by default
+        #
+        # The pykmip library stops you from deleting objects that have been activated.
+        # HashiCorp Vault does not support deactivating a object. Including these tests
+        # will create managed kmip objects in Vault without being able to delete them.
+        #
+        # operation_activate
+        #try:
+        #    result = client.activate(uid)
+        #    logger.info("Successfully activated: {0}".format(uid))
+
+        #    testResults["results"].append({"operationName": "operation_activate",
+        #                                   "successful": True})
+        #
+        #except Exception as e:
+        #    logger.error(e)
+        #    testResults["results"].append({"operationName": "operation_activate",
+        #                                   "successful": False})
+
+        # operation_revoke 
+        #try:
+        #    client.revoke(
+        #        enums.RevocationReasonCode.KEY_COMPROMISE,
+        #        uid=uid,
+        #        revocation_message="I want to revoke this secret.",
+        #        compromise_occurrence_date=int(time.time())
+        #    )
+        #    logger.info(
+        #        "Successfully revoked secret with ID: {0}".format(uid)
+        #    )
+
+        #    testResults["results"].append({"operationName": "operation_revoke",
+        #                                   "successful": True})
+        #except Exception as e:
+        #    logger.error(e)
+        #    testResults["results"].append({"operationName": "operation_revoke",
+        #                                   "successful": False})
 
         # operation_get
         try:
@@ -84,7 +102,6 @@ if __name__ == '__main__':
             logger.error(e)
             testResults["results"].append({"operationName": "operation_get",
                                            "successful": False})
-
         # operation_get_attributes
         try:
             _, attributes = client.get_attributes(uid, attribute_names=["State"])
@@ -100,25 +117,6 @@ if __name__ == '__main__':
         except Exception as e:
             logger.error(e)
             testResults["results"].append({"operationName": "operation_get_attributes",
-                                           "successful": False})
-
-        # operation_revoke 
-        try:
-            client.revoke(
-                enums.RevocationReasonCode.KEY_COMPROMISE,
-                uid=uid,
-                revocation_message="I want to revoke this secret.",
-                compromise_occurrence_date=int(time.time())
-            )
-            logger.info(
-                "Successfully revoked secret with ID: {0}".format(uid)
-            )
-
-            testResults["results"].append({"operationName": "operation_revoke",
-                                           "successful": True})
-        except Exception as e:
-            logger.error(e)
-            testResults["results"].append({"operationName": "operation_revoke",
                                            "successful": False})
 
         # operation_get_attribute_list
@@ -139,11 +137,6 @@ if __name__ == '__main__':
 
         # operation_destroy
         try:
-            uid = client.create(
-                algorithm,
-                length,
-                operation_policy_name=opts.operation_policy_name
-            )
             client.destroy(uid)
             logger.info("Successfully destroyed secret with ID: {0}".format(
                 uid))
